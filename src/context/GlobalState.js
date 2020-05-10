2d5eb083f61e1ee7cd150b62bb9433903f228104
Jino C Jacob <jcjacob@salmonggn.com>
2020-05-11 00:22:42 +0530

    removed comments

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -6,32 +6,6 @@ import ItemsContext from 'context/items-context';
     const [items,setItems] = useState(JSON.parse(localStorage.getItem('items')) || [])
     const [userName,setUserName] = useState(localStorage.getItem('userName')||'' )
    
-    // const  setUserNameGlobal = user => {
-    //   setUserName(user)
-    // }
-
-  // const addProductToCart = product => {
-  //    setCart([...cart,product])
-  // }
-
-  // const modifyCart = (product,check) => {
-  //   const temp = [...cart]
-  //   var id = temp.findIndex(item => item.id === product.id );
-  //   if(check){
-  //     temp[id].quantity= temp[id].quantity+1
-  //   }
-  //   else {
-  //       temp[id].quantity= temp[id].quantity-1
-  //   }
-  //   setCart(temp)
-  // }
-
-  // const removeProductFromCart = productId => {
-  //   const temp = [...cart]
-  //   var id = temp.findIndex(product => product.id === productId );
-  //   temp.splice(id,1);
-  //   setCart(temp)
-  // }  
     return (
       <ItemsContext.Provider
         value={{
